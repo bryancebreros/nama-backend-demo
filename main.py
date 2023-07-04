ba9702930fd1fc58f49061aaf5160cb1aba7e1f8
Jesus Aguilera <aguilera.jesus@tftus.com>
2023-07-04 13:48:03 -0600

    upload and delete pdf files added

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from app.routes.ChatGPT import ChatGPT
+from app.routes.Files import Files
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(ChatGPT, "/question")
-
+# api.add_resource(Files, "/upload")
+# api.add_resource(Files, "/files/<string:file_name>")
+api.add_resource(Files, "/files", endpoint="upload")
+api.add_resource(Files, "/files/<string:file_name>", endpoint="delete")
 if __name__ == "__main__":
     app.run(debug=True)
     # app.run()
